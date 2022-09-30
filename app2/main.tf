af720bf2cfc6687f2e04db4d8fad548deaa0d465
Li Lin <li.lin@hashicorp.com>
2022-09-30 11:25:05 -0700

    Make two workspaces

---
+++ app2/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">=1.2"
+
+  cloud {
+    hostname = "app.staging.terraform.io"
+    organization = "lilin_staging"
+
+    workspaces {
+      name = "lilin-atlantis-with-tfc-2"
+    }
+  }
+}
+
+output "current_workspace_name" {
+  value = terraform.workspace
+}
+
+resource "null_resource" "example" {
+}
+
+resource "null_resource" "example2" {
+}
