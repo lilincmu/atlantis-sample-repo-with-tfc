956d011850a1b338f4ee4033ccf985199d126359
Li Lin <li.lin@hashicorp.com>
2022-11-22 11:59:47 -0800

    Update provider

--- main.tf
+++ main.tf
@@ -7,6 +7,17 @@ terraform {
       name = "lilin-atlantis-with-tfc"
     }
   }
+
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+provider "null" {
+  # Configuration options
 }
 
 resource "null_resource" "example" {
