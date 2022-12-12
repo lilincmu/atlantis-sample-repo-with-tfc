fb42098c1171f80938dffabd3f774cc6fda6e09f
Li Lin <lilincmu@gmail.com>
2022-12-12 15:53:51 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,13 @@ terraform {
       name = "lilin-atlantis-with-tfc"
     }
   }
+  
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.0"
+    }
+  }
 }
 
 provider "null" {
