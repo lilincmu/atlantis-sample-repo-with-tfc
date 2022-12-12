cd0d9c47b17e7fed0bfaffffef5d256545c4416f
Li Lin <lilincmu@gmail.com>
2022-12-12 15:48:21 -0800

    remove provider

--- main.tf
+++ main.tf
@@ -9,13 +9,6 @@ terraform {
       name = "lilin-atlantis-with-tfc"
     }
   }
-
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.2.0"
-    }
-  }
 }
 
 provider "null" {
